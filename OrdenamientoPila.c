9dc24eb1d93fe772f2f4af5de9ff8881d5a4c3ba
LSL <lorena.salazar4444@gmail.com>
2025-02-08 12:58:00 -0600

    Ejercicio 1: Mostrar el menor número de 5 (sin comentarios)

--- OrdenamientoPila.c
+++ OrdenamientoPila.c
@@ -9,8 +9,6 @@ int main(){
 	int i = 0, n;
 	
 	pila miPila = crearPila();
-	/*pila Pmenor = crearPila();
-	pila Pmenos = crearPila();*/
 	pila Paux   = crearPila();
 	
 	for(i=0; i<=4; i++){
@@ -27,64 +25,16 @@ int main(){
 		
 	}
 	
-	//numMenor(miPila, Paux);
-	
 	printf("\nSenior usuario de los numeros que introdujo, el menor es: %d", numMenor(miPila, Paux));
-
-	/*for(i=0; i<=3; i++){
-
-		if(top(miPila) < top(Paux)){
-			llenarPila(&Pmenor, sacarPila(&miPila));
-			sacarPila(&Paux);
-			printf("\n Pila menor1: %d", top(Pmenor));
-		}else{
-			llenarPila(&Pmenor, sacarPila(&Paux));
-			sacarPila(&miPila);
-			printf("\n Pila menor2: %d", top(Pmenor));
-		}
-	}
-	
-	Pmenos = Pmenor;
-	
-	printf("\n*******************\n");
-	
-	for(i=0; i<=3; i++){
-		llenarPila(&miPila, sacarPila(&Pmenor));
-		if(i==3){
-			continue;	
-		}else{
-			llenarPila(&Paux, sacarPila(&Pmenos));
-		}
-	}
-	
-
-	for(i=0; i<=2; i++){
-
-		if(top(miPila) < top(Paux)){
-			llenarPila(&Pmenor, sacarPila(&miPila));
-			sacarPila(&Paux);
-			printf("\n Pila menor1: %d", top(Pmenor));
-		}else{
-			llenarPila(&Pmenor, sacarPila(&Paux));
-			sacarPila(&miPila);
-			printf("\n Pila menor2: %d", top(Pmenor));
-		}
-	}
-
-	for(i=0; i<=3; i++){
-		printf("\n Pila menor1: %d", top(Pmenor));
-	}*/
 	
 	return 0;
 }
 
 int numMenor(pila miPila, pila Paux){
 	
-	//pila miPila = crearPila();
 	pila Pmenor = crearPila();
 	pila Pmenos = crearPila();
 	pila Pmenol = crearPila();
-	//pila Paux   = crearPila();
 	int x=3, flag=0, i=0, j=0, k=0;
 	
 	while(flag == 0){
@@ -100,8 +50,7 @@ int numMenor(pila miPila, pila Paux){
 				printf("\n Pila menor2: %d", top(Pmenor));
 			}
 		}
-		
-		//for(i=0; i<=x; i++){
+
 		Pmenol = Pmenor;
 		
 		j = top(Pmenos);
@@ -109,18 +58,15 @@ int numMenor(pila miPila, pila Paux){
 		if(j == k){
 			return j;
 			flag = 1;
-			//break;
 		}if(j == k+1){
 			return j -= 1;
 			flag = 1;
 		}if(top(Pmenor) == -1){
 			return top(Pmenos);
 		}
-		//}
-		
+
 		Pmenos = Pmenor;
 		printf("\n*******************\n");
-		//return top(Pmenor);
 		
 		for(i=0; i<=x; i++){
 			llenarPila(&miPila, sacarPila(&Pmenor));
